b70c63c9110e12d8872b3a64fc0c980129000158
Stanislaw Pietrzak <integralzerox@gmail.com>
2020-12-19 21:05:27 +0100

    Add string printing function
    Signed-off-by: Stanislaw Pietrzak <integralzerox@gmail.com>

--- ssd1306-font.c
+++ ssd1306-font.c
@@ -80,3 +80,72 @@ int ssd1306_print_char(struct ssd1306 *oled, int x, int y, char c)
 
 	return 0;
 }
+
+/**
+ * @brief
+ *     Prints an ASCII string at the line starting with x and y
+ *
+ * @param[IN] oled    pointer to SSD1306 main handle
+ * @param[IN] x       start of horizontal coordinate
+ * @param[IN] y       start of vertical coordinate
+ * @param[IN] str     ASCII string
+ *
+ * @return returns zero or negative error
+ */
+int ssd1306_print_str(struct ssd1306 *oled, int x, int y, const char* str)
+{
+	int err = 0;
+	int str_len;
+	const struct font_desc *font;
+	int total_char_width;
+	int font_width, font_height;
+	int avaible_space;
+	int char_num;
+
+	if (!oled || !str)
+		return -EPERM;
+
+	font = get_default_font(SSD1306_HORIZONTAL_MAX, SSD1306_VERTICAL_MAX,
+				DEFAULT_FONT_WIDTH, DEFAULT_FONT_HEIGHT);
+
+	if (!font) {
+		LOG(KERN_DEBUG, "Given font does not exist");
+		return -EPERM;
+	}
+
+	font_height = font->height;
+	font_width = font->width;
+	str_len = strlen(str);
+
+	//The total space in single line from first character to the end of line
+	avaible_space = SSD1306_HORIZONTAL_MAX - x;
+
+	if (y + font_height > SSD1306_VERTICAL_MAX) {
+		LOG(KERN_DEBUG, "No more space on the display."
+		    " Move the string a little higher");
+		return -EPERM;
+	}
+
+	/* Add +1 to font_width to provide one extra pixel free space beetwen
+	 * characters
+	 */
+	total_char_width = font_width + 1;
+	// Check if possible to entire whole string to the display
+	if (avaible_space < total_char_width * str_len) {
+		LOG(KERN_DEBUG, "ASCII string %s is too long: %d pixels"
+		    "over border", str,
+		    (total_char_width * str_len) - avaible_space);
+		return -EPERM;
+	}
+
+	for (char_num = 0; char_num < str_len; char_num++)
+	{
+		//Calculate offset for next character in line
+		const int offset = char_num * total_char_width;
+
+		//Try to print entire string
+		err |= ssd1306_print_char(oled, x + offset, y, str[char_num]);
+	}
+
+	return err;
+}
--- ssd1306-font.h
+++ ssd1306-font.h
@@ -1,3 +1,4 @@
 /* SPDX-License-Identifier: GPL-2.0 */
 
 int ssd1306_print_char(struct ssd1306 *oled, int x, int y, char c);
+int ssd1306_print_str(struct ssd1306 *oled, int x, int y, const char* str);
